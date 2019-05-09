aa58864710c7167c461ad8c823602de1779e7979
Quan Tang <quan.tang@live.com>
2019-05-09 15:17:12 +1200

    Add basic type declaration file to yang-js. (#93)
    Thanks, this is very useful!

---
+++ index.d.ts
@@ -0,0 +1,47 @@
+// Type definitions for yang-js
+// Project: yang-js
+// Definitions by: quan.tang
+
+export function parse(schema: string): YangInstance;
+export function compose(data: any): YangInstance;
+export function resolve(name: any): YangInstance;
+export function clear(): void;
+
+
+export interface YangInstance {
+    bind(obj: any): this;
+    eval(data?: any): YangModel;
+    validate(data: any): any;
+    extends(schema: string): this;
+}
+
+export interface YangModel extends YangProperty {
+    access(model: string): YangModel;
+    on(event: string, path: string, callback: any);
+}
+
+export interface YangProperty {
+    name: string;
+    parent: YangProperty;
+    path: YangXPath;
+    children: YangProperty[];
+    change: any[];
+
+    get(key?: string): any;
+    set(value: any): this;
+    merge(value: any): this;
+    create(data: any): this;
+    find(pattern?: string): YangProperty;
+    in(pattern?: string): YangProperty;
+    do(): Promise<any>;
+    toJSON(tag?: boolean): any;
+}
+
+export interface YangXPath {
+    tail: YangXPath;
+    contains(path: string): boolean;
+    locate(path: string): YangXPath;
+}
+
+export function _import(name: string): YangInstance;
+export {_import as import};
